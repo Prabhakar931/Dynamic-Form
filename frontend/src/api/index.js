3c1fbaaf32805bafc16079babfcaf4e72e2c6209
Prabhakar Srivastava <130163978+Prabhakar931@users.noreply.github.com>
2026-06-07 22:09:39 +0530

    Stage deployment fix

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://dynamic-form-backend-koe5.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
