621977c292503c6592c154ae30b83131ebd4538e
Prabhakar Srivastava <130163978+Prabhakar931@users.noreply.github.com>
2026-06-07 20:58:22 +0530

    Deployment fix for dynmaic form

--- frontend/server/server.cjs
+++ frontend/server/server.cjs
@@ -5,7 +5,7 @@ const path = require('path')
 
 const app = express()
 const PORT = process.env.PORT || 3001
-const API_URL = 'https://dynamic-from-backend.onrender.com'
+const API_URL = 'https://dynamic-from-backend.onrender.com/api'
 
 app.use(cors())
 app.use(express.json())
